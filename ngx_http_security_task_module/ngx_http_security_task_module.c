f25ec45a4764ce6fa997ec28463cfecb99206f2c
doughnut <54584786+doughnuty@users.noreply.github.com>
2020-07-26 04:40:42 +0600

    redone to filter

--- ngx_http_security_task_module/ngx_http_security_task_module.c
+++ ngx_http_security_task_module/ngx_http_security_task_module.c
@@ -1,7 +1,6 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
-
 #define I_FOUND 1
 #define AM_FOUND 2
 #define I_AM_FOUND 3
@@ -12,29 +11,31 @@
 
 
 typedef struct {
-    ngx_array_t  *redirects;
-} ngx_http_security_task_loc_conf_t;
+    ngx_flag_t enable;
+} ngx_http_security_task_conf_t;
 
-static char *ngx_http_security_task(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
-static ngx_int_t ngx_http_security_task_handler(ngx_http_request_t *r);
-static void * ngx_http_security_task_create_loc_conf(ngx_conf_t *cf);
-static char * ngx_http_security_task_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child);
+static void *ngx_http_security_task_create_loc_conf(ngx_conf_t *cf);
+static char *ngx_http_security_task_merge_loc_conf(ngx_conf_t *cf, void *parent,
+    void *child);
+static ngx_int_t ngx_http_security_task_header_filter(ngx_http_request_t *r, ngx_chain_t *in)
+static ngx_int_t ngx_http_security_task_init(ngx_conf_t *cf);
+
 
 static ngx_command_t  ngx_http_security_task_commands[] = {
 
     { ngx_string("security_task_if"),
-      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
-      ngx_http_security_task,
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_FLAG,
+      ngx_conf_set_flag_slot,
       NGX_HTTP_LOC_CONF_OFFSET,
-      0,
+      offsetof(ngx_http_security_task_conf_t, enable),
       NULL },
 	
-	ngx_null_command
+      ngx_null_command
 };
 
 static ngx_http_module_t  ngx_http_security_task_module_ctx = {
     NULL,                                   /* preconfiguration */
-    NULL,        /* postconfiguration */
+    ngx_http_security_task_init,            /* postconfiguration */
 
     NULL,
                                             /* create main configuration */
@@ -65,6 +66,17 @@ ngx_module_t  ngx_http_security_task_module = {
     NGX_MODULE_V1_PADDING
 };
 
+static ngx_int_t ngx_http_security_task_init(ngx_conf_t *cf)
+{
+    ngx_http_next_header_filter = ngx_http_top_header_filter;
+    ngx_http_top_header_filter = ngx_http_security_task_header_filter;
+
+    /*ngx_http_next_request_body_filter = ngx_http_top_request_body_filter;
+    ngx_http_top_request_body_filter = ngx_http_security_task_body_filter;
+    */
+    return NGX_OK;
+}
+
 int key_words_search(ngx_str_t line)
 {
 	int i_found = 0;
@@ -72,26 +84,23 @@ int key_words_search(ngx_str_t line)
 	int ha_found = 0;
 	int ret_value = 0;
 	unsigned int i = 0;
-	char target[7] = {'h', 'a', 'c', 'k', 'e', 'r', '\0'};
 	for(i = 0; i < line.len; i++)
 	{
-		switch (line.data[i])
+		switch (line[i])
 		{
 			case 'i':
 				if(i_found == 0) ret_value += 1;
 				break;
 			case 'a':
 				if(i + 1 != line.len)
-				{
+			{
 				if(line.data[i + 1] == 'm' && am_found == 0) 
-				{
 					ret_value += 2;
 					am_found += 1;
-					i+=1;
-				}
-				}
+			}
 				break;
 			case 'h':
+				char target[6] = "hacker";
 				if (line.len < i + 5 || ha_found != 0)
 					break;
 				int j;
@@ -102,43 +111,64 @@ int key_words_search(ngx_str_t line)
 						break;
 					else j++;
 				}
-				if(j == 6) {
-				ret_value += 4;
+				if(j == 6) ret_value += 4;
 				ha_found += 1;
-				}
 				break;
 		}
 	}
 	return ret_value;
 }
 
-
-static ngx_int_t 
-ngx_http_security_task_handler(ngx_http_request_t *r)
+static ngx_int_t
+ngx_http_security_task_header_filter(ngx_http_request_t *r, ngx_chain_t *in)
 {
-	if (key_words_search(r->uri) == ALL_FOUND)
-	{
-	    ngx_str_t  uri, args;
+    u_char                      *p;
+    ngx_chain_t                 *cl;
+    ngx_http_security_task_conf_t  *conf;
+    conf = ngx_http_get_module_loc_conf(r, ngx_http_security_task_filter_module);
+    if (!conf->enable) {
+    return ngx_http_next_request_header_filter(r, in);
+    }
+    ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
+               "search for the request key words");
+    if (key_words_search(r->uri) == ALL_FOUND) {
+    	ngx_str_t  uri, args;
 
-	    ngx_str_set(&uri, "/security");
-		ngx_str_set(&args, "");
+    	ngx_str_set(&uri, "/security");
+    	ngx_str_set(&args, "");
 
-	    return ngx_http_internal_redirect(r, &uri, &args);
-
-	}
-	return NGX_OK;
+    	ngx_http_internal_redirect(r, &uri, &args);
+    	ngx_http_finalize_request(r, NGX_DONE);
+    	return NGX_HTTP_MOVED_PERMANENTLY;
+     }
+     return ngx_http_next_request_header_filter(r, in);
 }
-
-static char *
-ngx_http_security_task(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+/*
+static ngx_int_t
+ngx_http_security_task_body_filter(ngx_http_request_t *r, ngx_chain_t *in)
 {
-	ngx_http_core_loc_conf_t  *clcf;
-
-	clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-	clcf->handler = ngx_http_security_task_handler;
-
-	return NGX_CONF_OK;
+    u_char                      *p;
+    ngx_chain_t                 *cl;
+    ngx_http_security_task_conf_t  *conf;
+    conf = ngx_http_get_module_loc_conf(r, ngx_http_security_task_filter_module);
+    if (!conf->enable) {
+    return ngx_http_next_request_body_filter(r, in);
 }
+    ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
+               "search for the request key words");
+    if (key_words_search(r->uri) == ALL_FOUND) {
+    	ngx_str_t  uri, args;
+
+    	ngx_str_set(&uri, "/security");
+    	ngx_str_set(&args, "");
+
+    	ngx_http_internal_redirect(r, &uri, &args);
+    	ngx_http_finalize_request(r, NGX_DONE);
+    	return NGX_HTTP_MOVED_PERMANENTLY;
+     }
+     return ngx_http_next_request_body_filter(r, in);
+}
+*/
 
 static void *
 ngx_http_security_task_create_loc_conf(ngx_conf_t *cf)
@@ -150,6 +180,7 @@ ngx_http_security_task_create_loc_conf(ngx_conf_t *cf)
         return NULL;
     }
 
+    conf->enable = NGX_CONF_UNSET;
     
     return conf;
 }
@@ -162,9 +193,7 @@ ngx_http_security_task_merge_loc_conf(ngx_conf_t *cf, void *parent,
     ngx_http_security_task_loc_conf_t  *prev = parent;
     ngx_http_security_task_loc_conf_t  *conf = child;
 
-    if (conf->redirects == NULL && prev->redirects) {
-        conf->redirects = prev->redirects;
-    }
+    ngx_conf_merge_value(conf->enable, prev->enable, 0);
 
     return NGX_CONF_OK;
 }
