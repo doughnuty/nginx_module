4cbab3da770578b2962d6b497aff89107c0dde11
doughnut <malika240502@gmail.com>
2020-08-03 13:18:05 +0000

    flag edited

--- ngx_http_redirect_all_module/ngx_http_redirect_all_module.c
+++ ngx_http_redirect_all_module/ngx_http_redirect_all_module.c
@@ -11,6 +11,7 @@ typedef struct {
 
 static ngx_int_t ngx_http_redirect_all_handler(ngx_http_request_t *r)
 {
+
 	ngx_http_redirect_all_conf_t *conf;
 	
 	conf = ngx_http_get_module_loc_conf(r, ngx_http_redirect_all_module);
@@ -40,17 +41,32 @@ ngx_http_redirect_all_init(ngx_conf_t *cf)
 	return NGX_OK;
 }
 
-/*
-static char *ngx_http_redirect_all(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+static void *
+ngx_http_redirect_all_create_loc_conf(ngx_conf_t *cf)
 {
-	ngx_http_core_loc_conf_t *clcf;
+    ngx_http_redirect_all_conf_t  *conf;
 
-	clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-	clcf->handler = ngx_http_redirect_all_handler;
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_redirect_all_conf_t));
+    if (conf == NULL) {
+        return NULL;
+    }
 
-	return NGX_CONF_OK;
+    conf->enable = NGX_CONF_UNSET;
+
+    return conf;
 }
-*/
+
+
+static char *
+ngx_http_redirect_all_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
+{
+    ngx_http_redirect_all_conf_t *prev = parent;
+    ngx_http_redirect_all_conf_t *conf = child;
+
+    ngx_conf_merge_value(conf->enable, prev->enable, 0);
+
+    return NGX_CONF_OK;
+}
 
 static ngx_http_module_t ngx_http_redirect_all_module_ctx = {
   NULL,                                 /* preconfiguration */
@@ -60,8 +76,8 @@ static ngx_http_module_t ngx_http_redirect_all_module_ctx = {
   NULL,                                 /* init main configuration */
   NULL,                                 /* create server configuration */
   NULL,                                 /* merge server configuration */
-  NULL,                                 /* create location configuration */
-  NULL                                  /* merge location configuration */
+  ngx_http_redirect_all_create_loc_conf,                                 /* create location configuration */
+  ngx_http_redirect_all_merge_loc_conf                                  /* merge location configuration */
 };
 
 static ngx_command_t  ngx_http_redirect_all_commands[] = {
