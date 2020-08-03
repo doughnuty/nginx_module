75b9c1d4b34aeb393b79ff020a4651c6ee3f9e76
doughnut <malika240502@gmail.com>
2020-08-03 12:07:14 +0000

    segfault

--- ngx_http_redirect_all_module/ngx_http_redirect_all_module.c
+++ ngx_http_redirect_all_module/ngx_http_redirect_all_module.c
@@ -3,15 +3,22 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 
-//ngx_module_t ngx_http_redirect_all_module;
-/*
+ngx_module_t ngx_http_redirect_all_module;
+
 typedef struct {
     ngx_flag_t         enable;
 } ngx_http_redirect_all_conf_t;
-*/
 
 static ngx_int_t ngx_http_redirect_all_handler(ngx_http_request_t *r)
 {
+	ngx_http_redirect_all_conf_t *conf;
+	
+	conf = ngx_http_get_module_loc_conf(r, ngx_http_redirect_all_module);
+
+	if(!conf->enable) {
+		return NGX_DECLINED;
+	}
+
 	ngx_table_elt_t *h;
 	h = ngx_list_push(&r->headers_out.headers);
 	h->hash = 1;
@@ -22,37 +29,18 @@ static ngx_int_t ngx_http_redirect_all_handler(ngx_http_request_t *r)
 }
 
 
-/*static ngx_int_t
+static ngx_int_t
 ngx_http_redirect_all_init(ngx_conf_t *cf)
 {
-	ngx_http_handler_pt *h;
-	ngx_http_core_main_conf_t *cmcf;
+	ngx_http_core_loc_conf_t *clcf;
 
-	cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
-
-	h = ngx_array_push(&cmcf->phases[NGX_HTTP_REWRITE_PHASE].handlers);
- 	if (h == NULL) {
-	    return NGX_ERROR;
-	}
-
-  	*h = ngx_http_redirect_all_handler;
+	clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+	clcf->handler = ngx_http_redirect_all_handler;
 
 	return NGX_OK;
 }
 
-*/
-static ngx_http_module_t ngx_http_redirect_all_module_ctx = {
-  NULL,                                 /* preconfiguration */
-  NULL,           /* postconfiguration */
-
-  NULL,                                 /* create main configuration */
-  NULL,                                 /* init main configuration */
-  NULL,                                 /* create server configuration */
-  NULL,                                 /* merge server configuration */
-  NULL,                                 /* create location configuration */
-  NULL                                  /* merge location configuration */
-};
-
+/*
 static char *ngx_http_redirect_all(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
 	ngx_http_core_loc_conf_t *clcf;
@@ -62,13 +50,27 @@ static char *ngx_http_redirect_all(ngx_conf_t *cf, ngx_command_t *cmd, void *con
 
 	return NGX_CONF_OK;
 }
+*/
+
+static ngx_http_module_t ngx_http_redirect_all_module_ctx = {
+  NULL,                                 /* preconfiguration */
+  ngx_http_redirect_all_init,           /* postconfiguration */
+
+  NULL,                                 /* create main configuration */
+  NULL,                                 /* init main configuration */
+  NULL,                                 /* create server configuration */
+  NULL,                                 /* merge server configuration */
+  NULL,                                 /* create location configuration */
+  NULL                                  /* merge location configuration */
+};
+
 static ngx_command_t  ngx_http_redirect_all_commands[] = {
 
     { ngx_string("redirect_all"),
-      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF,
-      ngx_http_redirect_all,
-      0,
-      0,
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_FLAG,
+      ngx_conf_set_flag_slot,
+      NGX_HTTP_LOC_CONF_OFFSET,
+      offsetof(ngx_http_redirect_all_conf_t, enable),
       NULL },
 
       ngx_null_command
