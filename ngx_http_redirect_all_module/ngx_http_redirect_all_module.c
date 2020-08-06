33dad4096c8297a0b6f33feab02cc49da047f9aa
doughnut <malika240502@gmail.com>
2020-08-06 20:41:36 +0000

    Almost working, need to fix number readings

--- ngx_http_redirect_all_module/ngx_http_redirect_all_module.c
+++ ngx_http_redirect_all_module/ngx_http_redirect_all_module.c
@@ -3,6 +3,65 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 
+typedef struct // Struct to hold which words were found
+{
+	int i_found;
+	int am_found;
+	int hacker_found;
+	
+	int all_found;
+
+} redirect_module_counter;
+
+static redirect_module_counter key_words_search(ngx_str_t line, redirect_module_counter counter)
+{
+        char target[7] = {'h', 'a', 'c', 'k', 'e', 'r', '\0'};
+
+        unsigned int i = 0;
+        for(i = 0; i < line.len; i++)
+        {
+                switch (line.data[i])
+                {
+                        case 'i':
+				counter.i_found += 1;
+				fprintf(stderr, "I character found in redirect module\n");
+                                break;
+                        case 'a':
+                                if(i + 1 != line.len)
+                                {
+                                	if(line.data[i + 1] == 'm' && counter.am_found == 0)
+                                    	{
+		                                fprintf(stderr, "AM key word found in redirect module\n");
+     						counter.am_found += 1;
+						i++;
+					}
+                                }
+                                break;
+                        case 'h':
+                                if (line.len < i + 5 || counter.hacker_found != 0)
+                                        break;
+                                int j;
+                                fprintf(stderr, "h character found in redirect module\n");
+                                for(j = 1; j < 6; j++)
+                                {
+                                        i++;
+                                        if(line.data[i] != target[j])
+                                                break;
+                                }
+
+                                if(j == 6) counter.hacker_found += 1;
+	                        if(j == 6) fprintf(stderr, "Hacker key word found in redirect module\n");
+                                break;
+                }
+        }
+
+	if(counter.i_found > 0 && counter.am_found > 0 && counter.hacker_found > 0)
+	{
+		counter.all_found = 1;
+	}
+        return counter;
+}
+
 ngx_module_t ngx_http_redirect_all_module;
 
 typedef struct {
@@ -14,39 +73,44 @@ static ngx_int_t ngx_http_redirect_all_handler(ngx_http_request_t *r)
 {
 
 	ngx_http_redirect_all_conf_t *conf;
-	
 	conf = ngx_http_get_module_loc_conf(r, ngx_http_redirect_all_module);
 
 	if(!conf->enable) {
 		return NGX_DECLINED;
 	}
 
-	ngx_table_elt_t *h;
-	h = ngx_list_push(&r->headers_out.headers);
-	h->hash = 1;
-	ngx_str_set(&h->key, "Location");
-	ngx_str_set(&h->value, "https://cybersec.kz/ru");
+	static redirect_module_counter counter;
+	counter = key_words_search(r->uri, counter);
 
-	conf->requestnum++;
+	if(counter.all_found == 1)
+	{
+		ngx_table_elt_t *location;
+		location = ngx_list_push(&r->headers_out.headers);
+		location->hash = 1;
+		ngx_str_set(&location->key, "Location");
+		ngx_str_set(&location->value, "https://cybersec.kz/ru");
 
-	fprintf(stderr, "detected %zd redirections to cybersec\n", conf->requestnum);
+		ngx_http_clear_location(r);
+		r->headers_out.location = location;
 
-	return NGX_HTTP_MOVED_TEMPORARILY;
+		conf->requestnum++;
+
+		fprintf(stderr, "detected %zd redirections to cybersec\n", conf->requestnum);
+
+		return NGX_HTTP_MOVED_TEMPORARILY;
+	}
+
+	counter.i_found = 0;
+	counter.am_found = 0;
+	counter.hacker_found = 0;
+
+	return NGX_DECLINED;
 }
 
 
 static ngx_int_t
 ngx_http_redirect_all_init(ngx_conf_t *cf)
 {
-/*
-	ngx_http_core_loc_conf_t *clcf;
-
-	clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-	clcf->handler = ngx_http_redirect_all_handler;
-
-	return NGX_OK;
-
-*/
     ngx_http_handler_pt        *h;
     ngx_http_core_main_conf_t  *cmcf;
 
